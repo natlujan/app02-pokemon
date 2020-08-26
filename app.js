5ae3a5526973361e97aec16999dce15c80c2b020
natlujan <tayilujan@gmail.com>
2020-08-25 20:59:12 -0700

    first commit

---
+++ app.js
@@ -0,0 +1,77 @@
+const pokemon = {
+    sprite: document.getElementById('pokemon-image'),
+    name: document.getElementById('pokemon-name'),
+    types: document.getElementById('pokemon-types'),
+    abilities: document.getElementById('pokemon-abilities')
+}
+
+
+const GetPokemon = async ()=>{
+    const url = 'https://pokeapi.co/api/v2/pokemon/beedrill';
+
+    const data = await fetch(url);
+    const dataJson = await data.json();
+
+    const {sprites, name, types, abilities} = dataJson;
+
+    types.forEach(element => {
+        const {type} = element;
+        //console.log(type.name);
+        pokemon.types.innerHTML += `<li>${type.name}</li>`;
+    });
+
+    abilities.forEach(element => {
+        const{ability} = element;
+        //pokemon.abilities.innerHTML += `<li>${ability.name}</li>`;
+        //console.log(ability.url);
+        GetAbilityInfo(ability.url,  ability.name);
+
+        //console.log(pokemonAbilitesInfo);
+    });
+    
+    //console.log(await abilities);
+
+    pokemon.sprite.src = await sprites.front_default;
+    pokemon.name.innerHTML = await `Name: ${name}`;
+}
+
+const GetAbilityInfo= async (url, abilityName)=>{
+
+    const data = await fetch(url);
+    const dataJson = await data.json();
+    const{effect_entries} = dataJson;
+
+    let effectsList = '';
+
+    effect_entries.forEach(element => {
+
+        const{effect, language} = element;
+
+        if(language.name === 'en'){
+            //console.log(effect);
+            effectsList += `<li>${effect}</li>`;
+        }
+    });
+
+    pokemon.abilities.innerHTML += 
+    `<li>
+        ${abilityName}
+        <div>effect</div>
+        <ul>
+            ${effectsList}
+        </ul>
+    </li>`;
+
+    /*const abilitiesPokemon = effect_entries.map( element =>{
+        const{effect, language} = element;
+
+        if(language.name === 'en'){
+            //console.log(effect);
+            return effect;
+        }
+    });*/
+
+    //console.log(abilitiesPokemon);
+}
+
+GetPokemon();
